485f030fe680c25a9d5916bfbc559a50eebc3c46
nathan-hadley <nathan.hadley@limblecmms.com>
2024-06-16 15:39:51 -0700

    Update version and about text

--- Cirque/Cirque/AboutView.swift
+++ Cirque/Cirque/AboutView.swift
@@ -8,17 +8,18 @@
 import SwiftUI
 
 struct AboutView: View {
-    let gitHubText = "The code for this project can be found at on [GitHub](https://github.com/nathan-hadley/cirque-ios)."
+    let gitHubText = "The code for this project can be found at [GitHub](https://github.com/nathan-hadley/cirque-ios)."
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
             Text("About")
                 .font(.largeTitle)
                 .padding()
             
-            Text("After a trip to Fontainebleau, France, I was inspired to bring their concept of bouldering circuits to Leavenworth. This app will allow the creation and sharing of circuits. We are starting with one V0-V3 Swiftwater circuit but more will be added soon.")
+            Text("After a trip to Fontainebleau, France, I was inspired to bring their concept of bouldering circuits to Leavenworth. This app will allow the creation and sharing of circuits. We are starting with one V0-V3 Swiftwater circuit and a V4-V9 Straightaways circuit but more will be added soon. Forestland is next!")
                 .padding()
                 
-            Text("If you're a developer, please reach out about contributing. If you're not a developer but want to submit a circuit, please send me a list of the problems and photos of each boulder, one blank and one with route line(s). I will also need coordinates for each boulder (not each problem).")
+            Text("If you're a developer, please reach out about contributing. If you're not a developer but want to help, please also reach out. Collecting all the information to add a new circuit takes time!")
                 .padding()
             
             Text(.init(gitHubText))
