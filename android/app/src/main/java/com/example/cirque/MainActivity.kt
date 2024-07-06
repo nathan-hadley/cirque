a2903a04fce8b179d5d250ca713154d0c57dfae3
Nathan Hadley <nathan.hadley@me.com>
2024-07-06 12:13:23 -0700

    redo android tabs

--- android/app/src/main/java/com/example/cirque/MainActivity.kt
+++ android/app/src/main/java/com/example/cirque/MainActivity.kt
@@ -1,33 +1,96 @@
 package com.example.cirque
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
-import com.example.cirque.databinding.ActivityMainBinding
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Place
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.cirque.ui.theme.CirqueTheme
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CirqueTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainContent(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+@Composable
+fun MainContent(modifier: Modifier = Modifier) {
+    var selectedTabIndex by remember { mutableStateOf(0) }
+    val tabs = listOf(
+        TabItem("Map", Icons.Filled.Place),
+        TabItem("About", Icons.Filled.Info)
+    )
 
-        val navView: BottomNavigationView = binding.navView
+    Column(modifier = modifier) {
+        TabRow(selectedTabIndex = selectedTabIndex) {
+            tabs.forEachIndexed { index, tabItem ->
+                Tab(
+                    selected = selectedTabIndex == index,
+                    onClick = { selectedTabIndex = index },
+                    text = { Text(tabItem.title) },
+                    icon = { Icon(tabItem.icon, contentDescription = tabItem.title) }
+                )
+            }
+        }
+        when (selectedTabIndex) {
+            0 -> MapPage()
+            1 -> AboutPage()
+        }
+    }
+}
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.navigation_home, R.id.navigation_dashboard)
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+@Composable
+fun MapPage() {
+    Text(
+        text = "Welcome to the Map Page",
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    )
+}
+
+@Composable
+fun AboutPage() {
+    Text(
+        text = "Welcome to the About Page",
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    )
+}
+
+data class TabItem(val title: String, val icon: androidx.compose.ui.graphics.vector.ImageVector)
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CirqueTheme {
+        MainContent()
     }
 }
