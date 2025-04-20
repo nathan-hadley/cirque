0e60cf053182473451670a9b790284df67912c6d
Nathan Hadley <nathan.hadley@limblecmms.com>
2025-04-20 07:50:57 -0700

    Remove react-native dir

--- react-native/constants/Config.ts
+++
@@ -1,7 +0,0 @@
-export const Config = {
-  MAPBOX_ACCESS_TOKEN:
-    'pk.eyJ1IjoibmF0aGFuaGFkbGV5IiwiYSI6ImNsdzdhdzd0ZTF3dXoya3Bld2E2NGlqenoifQ.J1OFj66gEQPKKy3aVCXxOw',
-  MAP_STYLE_URL: 'mapbox://styles/mapbox/streets-v12',
-  DEFAULT_ZOOM_LEVEL: 12,
-  DEFAULT_COORDINATES: [-120.6625, 47.5962], // Leavenworth coordinates
-};
