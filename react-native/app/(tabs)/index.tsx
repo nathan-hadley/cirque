cab2c8fa013e2c95646223c9574c67bad6cbee03
Nathan Hadley <nathan.hadley@limblecmms.com>
2025-05-04 11:28:58 -0700

    Map with initial zoom is working!

--- react-native/app/(tabs)/index.tsx
+++ react-native/app/(tabs)/index.tsx
@@ -1,10 +1,25 @@
-import { Text } from '@/components/ui/text';
 import { View } from 'react-native';
+import Mapbox, { Camera, MapView } from '@rnmapbox/maps';
+
+Mapbox.setAccessToken(
+  'pk.eyJ1IjoibmF0aGFuaGFkbGV5IiwiYSI6ImNsdzdmdXAxdDIycmoyanA3cXVvbHFxenQifQ.1wcBxvOkH8eU-6ev7SoO8Q'
+);
 
 export default function MapScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text>Map</Text>
+    <View style={{ flex: 1 }}>
+      <MapView
+        style={{ flex: 1 }}
+        styleURL="mapbox://styles/nathanhadley/clw9fowlu01kw01obbpsp3wiq"
+        compassEnabled={true}
+        scaleBarEnabled={true}
+      >
+        <Camera
+          centerCoordinate={[-120.713, 47.585]}
+          zoomLevel={10.5}
+          animationDuration={0}
+        />
+      </MapView>
     </View>
   );
 }
