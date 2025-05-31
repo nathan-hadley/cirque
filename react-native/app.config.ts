9c52048f5590d2e2cc878d9b946ede817236b071
Nathan Hadley <nathan.hadley@limblecmms.com>
2025-05-31 13:08:16 -0700

    Use app.config.ts

---
+++ react-native/app.config.ts
@@ -0,0 +1,55 @@
+import { ExpoConfig, ConfigContext } from 'expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: "Cirque",
+  slug: "Cirque",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "myapp",
+  userInterfaceStyle: "automatic",
+  newArchEnabled: true,
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: "com.nathan-hadley.Cirque"
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff"
+    }
+  },
+  web: {
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png"
+  },
+  plugins: [
+    "expo-router",
+    [
+      "expo-splash-screen",
+      {
+        image: "./assets/images/splash-icon.png",
+        imageWidth: 200,
+        resizeMode: "contain",
+        backgroundColor: "#ffffff"
+      }
+    ],
+    [
+      "@rnmapbox/maps",
+      {
+        RNMapboxMapsDownloadToken: process.env.MAPBOX_ACCESS_TOKEN
+      }
+    ],
+    [
+      "expo-location",
+      {
+        locationAlwaysAndWhenInUsePermission: "$(PRODUCT_NAME) requires your location to enable GPS navigation between boulder problems. For example, you will be able to see your location on the map and how close you are to a boulder."
+      }
+    ]
+  ],
+  experiments: {
+    typedRoutes: true
+  }
+}); 
