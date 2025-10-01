6505f6965a69ea622e6eee35eea7241a70c2aa6d
Nathan Hadley <nathanhadley@Nathans-MacBook-Pro.local>
2025-09-30 20:33:00 -0700

    add build and submit workflow

--- react-native/app.config.ts
+++ react-native/app.config.ts
@@ -4,7 +4,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: "Cirque",
   slug: "Cirque",
-  version: "1.2.0",
+  version: "1.3.0",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "myapp",
