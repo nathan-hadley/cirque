affc56878eed23010b271048296dd6df12b2dd0d
Nathan Hadley <nathan.hadley@me.com>
2025-10-29 07:05:24 -0700

    bump version

--- react-native/app.config.ts
+++ react-native/app.config.ts
@@ -6,7 +6,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
   name: IS_DEV ? "Cirque Dev" : "Cirque",
   slug: "Cirque",
-  version: "1.3.1",
+  version: "1.4.0",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "cirque",
