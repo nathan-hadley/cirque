40f0b7b6c387a282466caa04ccc94f9d2108079a
Nathan Hadley <nathan.hadley@me.com>
2025-10-30 06:45:35 -0700

    fix env

--- react-native/constants/api.ts
+++ react-native/constants/api.ts
@@ -1,5 +1,5 @@
-export const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL;
-export const API_KEY = process.env.EXPO_PUBLIC_API_KEY;
+export const API_BASE_URL = process.env.API_BASE_URL;
+export const API_KEY = process.env.API_KEY;
 
 export const API_ENDPOINTS = {
   submitProblem: `${API_BASE_URL}/v1/problems`,
