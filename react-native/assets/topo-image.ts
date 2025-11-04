4281c05184f8157258e54a8ff44195731a19aca2
Nathan Hadley <nathan.hadley@me.com>
2025-11-04 06:40:20 -0800

    Update topo-image.ts for Automation Test

--- react-native/assets/topo-image.ts
+++ react-native/assets/topo-image.ts
@@ -132,6 +132,7 @@ const topoImages = {
   "mad-meadows-the-undercling": require("@/assets/topos/mad-meadows-the-undercling.jpeg"),
   "mad-meadows-toprope-tough-guys": require("@/assets/topos/mad-meadows-toprope-tough-guys.jpeg"),
   "mad-meadows-trail-mix": require("@/assets/topos/mad-meadows-trail-mix.jpeg"),
+  "forestland-automation-test": require("@/assets/topos/forestland-automation-test.jpeg"),
 } as const;
 
 type TopoImageKey = keyof typeof topoImages;
